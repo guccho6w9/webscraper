60cb5a436a85cfcd5355007eb41c0fb77d7f781f
guccho6w9 <gusta.quiroga033@gmail.com>
2024-10-22 23:24:59 -0300

    Add(-): actualizado utils para traer una pagina y separar los productos para el paginado, implementado cache

--- scraping/utils.py
+++ scraping/utils.py
@@ -1,7 +1,13 @@
 import requests
+from django.core.cache import cache
 from bs4 import BeautifulSoup
 
 def get_offers():
+    cached_offers = cache.get('offers')
+    
+    if cached_offers:
+        return cached_offers
+    
     url = 'https://www.mercadolibre.com.ar/ofertas?container_id=MLA779357-3&category=MLA1648#deal_print_id=38647970-8e63-11ef-9a39-593bd032a5af&c_id=carouseldynamic-home&c_element_order=undefined&c_campaign=VER-MAS&c_uid=38647970-8e63-11ef-9a39-593bd032a5af'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
@@ -11,26 +17,18 @@ def get_offers():
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Encuentra todos los elementos de productos
         products_container = soup.find_all('div', class_='poly-card__content')
 
         offers = []
         for product in products_container:
             image_url = product.find_previous('img', class_='poly-component__picture lazy-loadable') 
-
             title_element = product.find('a', class_='poly-component__title')
-         
-            # Precio actual
             price_element = product.find('div', class_='poly-price__current')
             price_fraction = price_element.find('span', class_='andes-money-amount__fraction') if price_element else None
-
-            # Precio original (dentro de la etiqueta <s>)
             og_price_element = product.find('s', class_='andes-money-amount')
             og_price_fraction = og_price_element.find('span', class_='andes-money-amount__fraction') if og_price_element else None
-
             discount_element = product.find('span', class_='andes-money-amount__discount')
-            link_element = title_element  # La misma etiqueta de título es el enlace
+            link_element = title_element  
 
             if not image_url or not title_element or not price_element or not og_price_element or not link_element:
                 continue  
@@ -38,12 +36,9 @@ def get_offers():
             image = image_url['data-src'] 
             price = price_fraction.get_text() if price_fraction else 'No disponible'
             og_price = og_price_fraction.get_text() if og_price_fraction else 'No disponible'
-   
-            og_price = og_price_element.get_text()
             discount = discount_element.get_text() if discount_element else 'Sin descuento'
             link = link_element['href']
 
-            print(title)
             offers.append({
                 'image': image,
                 'title': title,
@@ -53,6 +48,7 @@ def get_offers():
                 'link': link
             })
 
+        cache.set('offers', offers, timeout=60*15)  # Cachear por 15 minutos
         return offers
     else:
         return []
