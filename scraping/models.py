e4df37d7d0de19d22f599ab030e6d180a11f2649
guccho6w9 <gusta.quiroga033@gmail.com>
2024-10-23 19:55:01 -0300

    Add(-): actualizado el largo de la url en los modelos

--- scraping/models.py
+++ scraping/models.py
@@ -8,11 +8,11 @@ User = get_user_model()
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=3)  
-    url = models.URLField(max_length=400)
+    url = models.URLField(max_length=1000)
     store = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     date_scrapping = models.DateTimeField(auto_now_add=True)
-    img_url = models.URLField(max_length=400)
+    img_url = models.URLField(max_length=500)
     discount = models.CharField(max_length=10)
 
     saved_by = models.ManyToManyField(User, blank=True, related_name='saved_products')
