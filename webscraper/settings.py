7a38f35fe8fc2947fcb949130781769c96ecf218
guccho6w9 <gusta.quiroga033@gmail.com>
2024-10-22 23:26:41 -0300

    Add(-): actualizado settings con pagina de login default a homepage y agregado cache

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOGIN_REDIRECT_URL = '/ofertas/'
+LOGIN_REDIRECT_URL = '/home'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -117,11 +117,22 @@ USE_I18N = True
 
 USE_TZ = True
 
+# settings.py
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "scraping/static",  # Esta es la ubicación principal de tus archivos estáticos
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
